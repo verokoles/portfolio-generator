c72b372aafca8381fbefba4d95af370b031dab2d
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-03-29 20:44:39 -0500

    add template

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,4 @@
-
-const generatePage = (name, github) => {
+module.exports = (name, github) => {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -17,4 +16,4 @@ const generatePage = (name, github) => {
      </html>
      `;
     };
-    module.exports = generatePage;
+   
