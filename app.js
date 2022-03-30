e04c608811908905aa3741b158d4052ac8df196a
VeroKoles <97703518+verokoles@users.noreply.github.com>
2022-03-30 17:10:54 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
-console.log(name, github);
+// const pageHTML = generatePage(name, github);
 
-const pageHTML = generatePage(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw (err);
 
+//     console.log('Portfolio complete! Check out index.html to see the output!');
 
-
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-
-});
+// });
